8a311e2cb1659087044fc337bf1f8d7b953261e8
Bahtiyorjon Arslonbekov <bahtiyorjonq777@gmail.com>
2022-01-07 01:38:47 +0100

    video placed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Header from "./components/Header";
+import BgVideo from "./components/BgVideo";
+import styled from "styled-components";
+
 function App() {
-  return <div className="App">Header</div>;
+  return (
+    <div className="App">
+      <FrontWrap>
+        <BgVideo />
+        <Header />
+      </FrontWrap>
+    </div>
+  );
 }
+
+const FrontWrap = styled.div`
+  height: auto;
+  min-height: 100vh;
+`;
 
 export default App;
